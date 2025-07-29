2ce36043ac0e5de8378d250895b420be6e46708b
islom-sattorov <slmsttrv1@gmail.com>
2025-07-29 22:33:26 +0500

    count unique chars in user input with getchar/putchar then create based on this information histogram

--- histograms.c
+++ histograms.c
@@ -2,9 +2,18 @@
 #include <stdio.h>
 
 void print_array(int array[], int *size) {
-    printf("Print array: \n");
+    printf("\n Print array: \n");
     for(int i = 0; i < *size; i++) {
       printf("%d ", array[i]);
+    }
+
+    printf("\n");
+}
+
+void print_array_char(char array[], int *size) {
+    printf("Print array: \n");
+    for(int i = 0; i < *size; i++) {
+      printf("%c ", array[i]);
     }
 
     printf("\n");
@@ -82,18 +91,45 @@ void vertical_histogram(int array[], int *size) {
   }
 }
 
+int find_element(int array[], int *size, int target) {
+    for(int i = 0; i < *size; i++) {
+      if(array[i] == target) {
+        return i;
+      }
+    }
+
+    return 0;
+}
+
+
 int main() {
+  // Logic 2
   int c;
-  int idx = 0;
-  int ndigit[] = {2, 3, 9, 1, 2, 9, 8, 7 ,6, 4};
 
-  // 10 / 4;
-  int n = sizeof(ndigit) / sizeof(ndigit[0]);
+  int elems[30];
+  int ndigit[30];
+  int n = 30;
 
+  int i = 0;
 
-  print_array(ndigit, &n);
-  horizontal_histogram(ndigit, &n);
+  while ((c = getchar()) != EOF) {
+    int isFinded = find_element(elems, &n, putchar(c));
+
+    if(isFinded == 0) {
+      elems[i] = putchar(c);
+      ndigit[i] = 1;
+      i = i + 1;
+    }else {
+      ndigit[isFinded] = ++ndigit[isFinded];
+    }
+  }
   vertical_histogram(ndigit, &n);
+
+  // Logic 2
+  // int ndigit[] = {2, 3, 9, 1, 2, 9, 8, 7 ,6, 4};
+  // int n = sizeof(ndigit) / sizeof(ndigit[0]);
+  // print_array(ndigit, &n);
+  // horizontal_histogram(ndigit, &n);
 
   return 0;
 }
