194a78c71a7018ae881d56b2f75670ce7ca7418a
islom-sattorov <slmsttrv1@gmail.com>
2025-08-10 21:59:48 +0300

    functions

---
+++ functions.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int power(int, int);
+double fahrenheitToCelsius(int fahr);
+double celsiusToFahrenheit(int cel);
+
+int main(){
+  for (int i = 0; i < 10; ++i){
+    printf("%d %d %d\n", i, power(2, i), power(-3, i));
+  }
+
+  for(int i = 0; i < 10; ++i){
+    printf("%d %f\n", i, fahrenheitToCelsius(i));
+  }
+
+  for(int i = 0; i < 10; ++i){
+    printf("%d %f\n", i, celsiusToFahrenheit(i));
+  }
+  return 0;
+}
+
+double celsiusToFahrenheit(int cel) {
+  return 9.0 / 5.0 * cel + 32;
+}
+
+double fahrenheitToCelsius(int fahr) {
+  return (5.0 / 9.0) * (fahr - 32);
+}
+
+// power: raise base to n-th power; n >= 0
+int power(int base, int n){
+  int p = 1;
+
+  for(int i = 1; i <= n; ++i) {
+    p = p * base;
+  }
+
+  return p;
+}
