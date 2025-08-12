82e84eb20b7c43d5e9cf02ae9bff70d94a327b66
islom-sattorov <slmsttrv1@gmail.com>
2025-08-12 15:17:55 +0300

    1.20: detab function

--- finalFirstChapter.c
+++ finalFirstChapter.c
@@ -4,30 +4,43 @@
 #define TABSPACE 7
 
 int getLine(char line[], int maxline);
+int detab(char line[], int idx);
 
 int main() {
   int len;
   char line[MAXCHARACTER];
 
   while((len = getLine(line, MAXCHARACTER)) > 0) {
-     printf("Original Word: %s \n", line);
+     printf("Detabed line: %s \n", line);
   }
 
   return 0;
 }
 
+int detab(char line[], int idx) {
+  int spaces = TABSPACE - (idx % TABSPACE);
+
+  while(spaces-- > 0) {
+    line[idx++] = ' ';
+  }
+
+  return idx;
+}
 
 int getLine(char s[], int lim) {
   int c;
   int i = 0;
 
   for(; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
-    s[i] = c;
+    if(c == '\t') {
+     i = detab(s, i) - 1;
+    }else {
+      s[i] = c;
+    }
   }
 
   if(c == '\n'){
-    s[i] = c;
-    ++i;
+    s[i++] = c;
   }
 
   // \0 = null character, маркер того что строка закончилась
